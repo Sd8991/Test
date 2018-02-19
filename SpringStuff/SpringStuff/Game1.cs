73153d15a1ddc4085ebca1e48f380b64aa4ca456
Rickkerdt <r.goossens2@students.uu.nl>
2018-02-19 17:41:17 +0100

    Al mijn werk van vandaag. Vgm doen de springs t redelijk. Ik ben nog s gaan kijken naar hoe de veer constante berekend word, en vgm heb ik iets gevonden + geïmplementeerd, en vgm werkt t hoe t moet werken. We moeten alleen wel alles nog zien te schalen zodat t binnen t scherm blijft. Groter scherm of kleinere veren enzo. Anyhow, Vordtgang en P R O G R E S S

--- SpringStuff/SpringStuff/Game1.cs
+++ SpringStuff/SpringStuff/Game1.cs
@@ -9,7 +9,7 @@ public class Game1 : Game
     SpriteBatch spriteBatch;
     Spring s1, s2, s3;
     Rectangle r;
-    Texture2D DatPic;
+    Texture2D SpringPic;
     Spring[] allSpring;
     Rectangle[] allRectangle;
 
@@ -25,11 +25,11 @@ public class Game1 : Game
     {
         allSpring = new Spring[3];
         allRectangle = new Rectangle[3];
-        s1 = new Spring(new Vector2(50, 50), new Vector2(50, 100), 200.0f, 200.0f, 1, 50, 0.0f);
+        s1 = new Spring(new Vector2(50, 50), new Vector2(50, 100), 200.0f, 200.0f/*, 1*/, 50, 0.0f, 10, 5);
         allSpring[0] = s1;
-        s2 = new Spring(new Vector2(200, 100), new Vector2(100, 200), 100.0f, 100.0f, 1, 25, 0.0f);
+        s2 = new Spring(new Vector2(150, 50), new Vector2(100, 200), 200.0f, 200.0f, /*10,*/ 50, 0.0f, 10, 10);
         allSpring[1] = s2;
-        s3 = new Spring(new Vector2(400, 150), new Vector2(50, 100), 50.0f, 50.0f, 1, 100, 0.0f);
+        s3 = new Spring(new Vector2(250, 50), new Vector2(50, 100), 200.0f, 200.0f, /*25,*/ 50, 0.0f, 10, 20);
         allSpring[2] = s3;
         base.Initialize();
     }
@@ -38,7 +38,7 @@ public class Game1 : Game
     {
         // Create a new SpriteBatch, which can be used to draw textures.
         spriteBatch = new SpriteBatch(GraphicsDevice);
-        DatPic = Content.Load<Texture2D>("DatPic");
+        SpringPic = Content.Load<Texture2D>("Spring");
     }
 
     protected override void UnloadContent()
@@ -52,9 +52,9 @@ public class Game1 : Game
             Exit();
 
         foreach (Spring s in allSpring)
-            s.Physics();
+            s.SLS(gameTime);
 
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < allSpring.Length; i++)
         {
             r = new Rectangle((int)allSpring[i].beginPoint.X, (int)allSpring[i].beginPoint.Y, (int)allSpring[i].radius, (int)allSpring[i].newLength);
             allRectangle[i] = r;
@@ -70,7 +70,7 @@ public class Game1 : Game
         // TODO: Add your drawing code here
         spriteBatch.Begin();
         foreach (Rectangle r in allRectangle)
-            spriteBatch.Draw(DatPic, r, Color.AntiqueWhite);
+            spriteBatch.Draw(SpringPic, r, Color.AntiqueWhite);
         spriteBatch.End();
         base.Draw(gameTime);
     }
--- SpringStuff/SpringStuff/Spring.cs
+++ SpringStuff/SpringStuff/Spring.cs
@@ -8,26 +8,43 @@ using Microsoft.Xna.Framework;
 class Spring
 {
     public Vector2 beginPoint, endPoint;
-    public float restLength, newLength, stiffness, radius;
-    public float stretch = 0.0f;
+    public double restLength, newLength, stiffness, radius, move, area, innerArea, length;
+    public double stretch = 0.0f;
+    double thyme = 0;
 
-    public Spring(Vector2 beginPoint, Vector2 endPoint, float restLength, float newLength, float stiffness, float radius, float stretch)
+    public Spring(Vector2 beginPoint, Vector2 endPoint, float restLength, float newLength/*, float stiffness*/, float radius, float stretch, int nrOfWinding, float materialThickness, float displacmentThingy = 1, float move = 0)
     {
         this.beginPoint = beginPoint;
         this.endPoint = endPoint;
         this.restLength = restLength;
-        this.stiffness = stiffness;
+        //this.stiffness = stiffness;
         this.radius = radius;
-        this.stretch = stretch;
+        this.stretch = stretch + 5;
+        this.move = move;
+        this.newLength = restLength;
+        area = Math.PI * radius;
+        innerArea = Math.PI * (radius - materialThickness);
+        length = area * nrOfWinding;
+        stiffness = (displacmentThingy * Math.Pow(materialThickness, 4)) / (8 * nrOfWinding * Math.Pow((area - innerArea) / 2, 3));
     }
 
-    public void Physics()
+    public void SLS(GameTime gameTime, float mass = 1.0f)//Simple Liniear Spring
     {
-        float mass = 5.0f;
-        float downForce = mass * 9.81f;
-        float upForce = -stiffness * stretch;
-        float move = downForce / stiffness + upForce;
-        newLength = restLength + move;
+        thyme += (double)gameTime.ElapsedGameTime.Milliseconds / 1000;
+        move = Math.Sin(thyme * Math.Sqrt(stiffness / mass));
+        newLength += move;
         stretch = newLength - restLength;
+    }
+
+    public void CodeStash()
+    {/*
+        double upForce = -stiffness * stretch;
+        double downForce = mass * 9.81f;
+        double resultForce = upForce - downForce;
+        double acc = stretch / ((thyme * thyme));
+        double resultMass = Math.Abs(resultForce / acc);
+        float mass = 0;
+    float damping = 1;
+    //float acc = downForce / mass;;*/
     }
 }
