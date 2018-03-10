99938e4454cc4f67a9f8b3738fbc1fa8a41e1824
Rick Goossens <r.goossens2@students.uu.nl>
2018-03-10 17:45:39 +0100

    Moar WiP. In zekere zin werkt alles voor 1 spring, ook met dampening enzo. Alleen heb ik t gevoel dat ik ergens een foutje heb gemaakt, aangezien de veren heel snel stoppen enzo, in tegenstelling tot andere simulaties. Hier moeten we dus nog ff samen naar kijken ofzo.

--- SpringStuff/SpringStuff/Game1.cs
+++ SpringStuff/SpringStuff/Game1.cs
@@ -7,7 +7,7 @@ public class Game1 : Game
 {
     GraphicsDeviceManager graphics;
     SpriteBatch spriteBatch;
-    Spring s1, s2, s3, s4, s5;
+    Spring s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12;
     Rectangle r;
     Texture2D SpringPic;
     Spring[] allSpring;
@@ -23,18 +23,32 @@ public class Game1 : Game
 
     protected override void Initialize()
     {
-        allSpring = new Spring[5];
-        allRectangle = new Rectangle[5];
-        s1 = new Spring(new Vector2(50, 50), new Vector2(50, 100), 45, 10, 0.0f, 1f);
+        allSpring = new Spring[12];
+        allRectangle = new Rectangle[12];
+        s1 = new Spring(new Vector2(100, 50), new Vector2(100, 200), 100, 20, 0.0f, 1f, 20, 0f);
         allSpring[0] = s1;
-        s2 = new Spring(new Vector2(200, 50), new Vector2(100, 200), 90, 10, 0.0f, 1f);
+        s2 = new Spring(new Vector2(200, 50), new Vector2(200, 200), 100, 20, 0.0f, 1f, 20, 0.1f);
         allSpring[1] = s2;
-        s3 = new Spring(new Vector2(350, 50), new Vector2(50, 100), 180, 10, 0.0f, 1f);
+        s3 = new Spring(new Vector2(300, 50), new Vector2(300, 200), 100, 20, 0.0f, 1f, 20, 0.2f);
         allSpring[2] = s3;
-        s4 = new Spring(new Vector2(500, 50), new Vector2(100, 200), 90, 20, 0.0f, 1f);
+        s4 = new Spring(new Vector2(400, 50), new Vector2(400, 200), 100, 20, 0.0f, 1f, 20, 0.3f);
         allSpring[3] = s4;
-        s5 = new Spring(new Vector2(650, 50), new Vector2(50, 100), 180, 20, 0.0f, 1f);
+        s5 = new Spring(new Vector2(500, 50), new Vector2(500, 200), 100, 20, 0.0f, 1f, 20, 0.4f);
         allSpring[4] = s5;
+        s6 = new Spring(new Vector2(600, 50), new Vector2(600, 200), 100, 20, 0.0f, 1f, 20, 0.5f);
+        allSpring[5] = s6;
+        s7 = new Spring(new Vector2(100, 250), new Vector2(100, 400), 100, 20, 0.0f, 1f, 20, 0.6f);
+        allSpring[6] = s7;
+        s8 = new Spring(new Vector2(200, 250), new Vector2(200, 400), 100, 20, 0.0f, 1f, 20, 0.7f);
+        allSpring[7] = s8;
+        s9 = new Spring(new Vector2(300, 250), new Vector2(300, 400), 100, 20, 0.0f, 1f, 20, 0.8f);
+        allSpring[8] = s9;
+        s10 = new Spring(new Vector2(400, 250), new Vector2(400, 400), 100, 20, 0.0f, 1f, 20, 0.9f);
+        allSpring[9] = s10;
+        s11 = new Spring(new Vector2(500, 250), new Vector2(500, 400), 100, 20, 0.0f, 1f, 20, 1f);
+        allSpring[10] = s11;
+        s12 = new Spring(new Vector2(600, 250), new Vector2(600, 400), 100, 20, 0.0f, 1f, 20, 1.1f);
+        allSpring[11] = s12;
         base.Initialize();
     }
 
@@ -56,11 +70,15 @@ public class Game1 : Game
             Exit();
 
         foreach (Spring s in allSpring)
-            s.SLS(gameTime);
+        {
+            //s.SLS(gameTime);
+            s.SLSMkII(gameTime);
+            //s.VertMove();
+        }
 
         for (int i = 0; i < allSpring.Length; i++)
         {
-            r = new Rectangle((int)allSpring[i].beginPoint.X, (int)allSpring[i].beginPoint.Y, (int)allSpring[i].radius, (int)allSpring[i].newLength);
+            r = new Rectangle((int)allSpring[i].beginPointX, (int)allSpring[i].beginPointY, (int)allSpring[i].radius, (int)(allSpring[i].stretch + allSpring[i].height));
             allRectangle[i] = r;
         }
         // TODO: Add your update logic here
--- SpringStuff/SpringStuff/Spring.cs
+++ SpringStuff/SpringStuff/Spring.cs
@@ -7,32 +7,54 @@ using Microsoft.Xna.Framework;
 
 class Spring
 {
-    public Vector2 beginPoint, endPoint;
-    public double restLength, newLength, stiffness, radius, move, area, innerArea, length;
+    public double beginPointX, endPointX, beginPointY, endPointY;
+    public double restLength, newLength, height, stiffness, radius, move, area, innerArea, length, mass;
+    public double criticDamp, actualDamp, dampFactor;
+    public double Fgravity, Fspring, Fres, acceleration, velocity;
     public double stretch = 0.0f;
     double thyme = 0;
 
-    public Spring(Vector2 beginPoint, Vector2 endPoint, int nrOfWinding/*, float stiffness*/, float radius, float stretch, float materialThickness, float displacmentThingy = 69000, float move = 0)
+    public Spring(Vector2 beginPoint, Vector2 endPoint, int nrOfWinding, float radius, float stretch, float materialThickness, float mass, float dampFactor, float displacmentThingy = 69000, float move = 0)
     {
-        this.beginPoint = beginPoint;
-        this.endPoint = endPoint;
+        this.beginPointX = beginPoint.X;
+        this.endPointX = endPoint.X;
+        this.beginPointY = beginPoint.Y;
         this.radius = radius;
-        this.stretch = stretch + 5;
+        this.stretch = stretch;
         this.move = move;
-        restLength = materialThickness * nrOfWinding;
+        this.mass = mass;
+        this.dampFactor = dampFactor;
+        height = materialThickness * nrOfWinding;
+        //this.endPointY = beginPoint.Y + height;
         this.newLength = restLength;
         area = Math.PI * radius;
         innerArea = Math.PI * (radius - materialThickness);
         length = area * nrOfWinding;
         stiffness = (displacmentThingy * Math.Pow(materialThickness, 4)) / (8 * nrOfWinding * Math.Pow((area - innerArea) / 2, 3));
+        this.restLength = beginPoint.Y + height + (mass * 9.81 / stiffness);
+        this.endPointY = beginPoint.Y + height;
     }
 
-    public void SLS(GameTime gameTime, float mass = 5.0f)//Simple Liniear Spring
+    public void SLS(GameTime gameTime, float mass)//Simple Liniear Spring using Mathematics
     {
-        thyme += (double)gameTime.ElapsedGameTime.Milliseconds / 1000;
+        thyme += gameTime.ElapsedGameTime.Milliseconds / 1000;
         move = Math.Sin(thyme * Math.Sqrt(stiffness / mass));
         newLength += move;
         stretch = newLength - restLength;
+    }
+
+    public void SLSMkII(GameTime gameTime, float mass = 0.5f, float gravity = 9.81f)//Simple Liniear Spring using Laws of Physics
+    {
+        criticDamp = Math.Sqrt(mass * stiffness) * 2;
+        actualDamp = dampFactor * criticDamp;
+        Fgravity = mass * gravity;
+        Fspring = -stiffness * stretch;
+        Fres = Fgravity + Fspring - (actualDamp * velocity);
+        acceleration = Fres / mass;
+        thyme = (double)gameTime.ElapsedGameTime.Milliseconds / 1000;
+        velocity = velocity + acceleration * thyme;
+        endPointY = endPointY + velocity * thyme;
+        stretch = endPointY - restLength;
     }
 
     public void CodeStash()
@@ -45,5 +67,16 @@ class Spring
         float mass = 0;
     float damping = 1;
     //float acc = downForce / mass;;*/
+
+        /*public void VertMove()
+{
+    float oldBPoint = beginPoint.Y;
+    if (beginPoint.Y >= vMoveLowerLimit) moveDown = false;
+    if (beginPoint.Y <= vMoveUpperLimit) moveDown = true;
+    if (moveDown) beginPoint.Y++;
+    if (!moveDown) beginPoint.Y--;
+    newLength += beginPoint.Y - oldBPoint;
+    stretch = newLength - restLength;
+}*/
     }
 }
